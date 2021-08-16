b11f32c4527b0332cf7b797e805804bacfe32f41
BulbaWarrior <vladkalm49@gmail.com>
2021-08-17 01:04:56 +0300

    Create python application

---
+++ app_python/time.py
@@ -0,0 +1,29 @@
+from wsgiref.simple_server import make_server
+
+import falcon
+from datetime import datetime, timezone, timedelta
+
+MOSCOW_TZ = moscow_timezone = timezone(timedelta(hours=3))
+
+
+class TimeResource:
+    def get_time_string(self):
+        return datetime.now(tz=MOSCOW_TZ).strftime("%H:%M:%S")
+
+    def on_get(self, req, resp):
+        resp.status = falcon.HTTP_200
+        resp.content_type = falcon.MEDIA_TEXT
+        resp.text = f"The current time is: {self.get_time_string()}"
+
+
+app = falcon.App()
+
+time = TimeResource()
+
+app.add_route("/time", time)
+
+if __name__ == "__main__":
+    with make_server("", 8000, app) as httpd:
+        print("Serving on port 8000...")
+
+        httpd.serve_forever()
