767573786a051d9d5d5f2d08b03d85bff97ecb07
Vladislav Kalmykov <vkalmykov@provectus.com>
2021-09-07 18:37:40 +0300

    initial ansible commit

--- terraform/modules/instances/main.tf
+++ terraform/modules/instances/main.tf
@@ -8,20 +8,5 @@ resource "aws_instance" "app_server" {
   count                       = var.instance_count
   tags                        = var.tags
 
-
-  # use remote-exec to ensure the instances are in running state before creating dependent resources
-  provisioner "remote-exec" {
-    inline = [
-      "curl -fsSL https://get.docker.com | /bin/sh",
-      "sudo docker run -d --rm --name jenkins -p 8080:8080 -p 50000:50000 -u 0 -v `pwd`:/var/jenkins_home  -v /var/run/docker.sock:/var/run/docker.sock ${var.jenkins_image}"
-    ]
-
-    connection {
-      type        = "ssh"
-      user        = "ubuntu"
-      private_key = file(var.ssh_key_private)
-      host        = self.public_ip
-    }
-  }
 }
 
